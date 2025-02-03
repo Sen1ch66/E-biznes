2f66750698b5df57fff1ee2d3835b8b1f852e347
Arsenii06 <arsenzahovajko@gmail.com>
2025-02-03 15:57:01 +0100

    oh yes

--- scripts/scripts_login.js
+++ scripts/scripts_login.js
@@ -4,74 +4,66 @@ const LogRegForm = document.querySelector("form")
 const signBtn = document.querySelector(".btn-secondary")
 const logBtn = document.querySelector(".btn-primary")
 const deteleBtn = document.querySelector(".btn-danger")
-function renderP(){ // функція появи повідомлення про зареєстрований акаунт
+function renderMessage(text, color) { // функція появи повідомлення про зареєстрований акаунт (або щось інше)
     const message = document.createElement("p")
-    message.innerHTML = 'Konto zostalo zalozone'
-    message.style.color = 'green'
+    message.innerHTML = `<strong>${text}</strong>`
+    message.style.color = color
     LogRegForm.appendChild(message)
-    setTimeout(()=>{
-        message.remove() //видалення повідомлення через 5 секунд
-    }, 5000)
+    setTimeout(() => {
+        message.remove() //видалення повідомлення через 4 секунди
+    }, 4000)
 }
-function deleteAccMessage(){
-    const deleteMes = document.createElement("p")
-    deleteMes.textContent = "takiego konta nie istnieje"
-    deleteMes.classList.add("deleteAcc")
-    LogRegForm.appendChild(deleteMes)
-    setTimeout(()=>{deleteMes.remove()},4000)
-}
-signBtn.addEventListener("click", ()=>{ 
+signBtn.addEventListener("click", () => {
     const loginText = document.querySelector(".login").value
     const pass = document.querySelector(".password").value
-    listaKontow.push({login: loginText, password: pass})
-    if (!document.querySelector("p")){ // якщо є повідомлення про зареєстрований ак щоб 2 не повлялось
-        renderP()
+    if (loginText.trim() == '' || pass.trim() == '') { // перевірка чи всі поля для входу заповнені (trim прибирає пробіли)
+        renderMessage("Wpisz wszystkie dane", 'red')
+    } else {
+        listaKontow.push({ login: loginText.trim(), password: pass.trim()})
+        renderMessage('Konto zostalo zalozone', 'green')
     }
     console.log(listaKontow)
 })
-deteleBtn.addEventListener("click", ()=>{
+deteleBtn.addEventListener("click", () => {
     const loginText = document.querySelector(".login").value
     const pass = document.querySelector(".password").value
-    if (listaKontow.length === 0){
-        deleteAccMessage()
+    if (listaKontow.length === 0) {
+        renderMessage("Takiego konta nie istnieje", "red")
+        return
     }
     let foundAcc = false
-    for(let i in listaKontow){
-        if (listaKontow[i].login == loginText && listaKontow[i].password == pass){
+    for (let i in listaKontow) {
+        if (listaKontow[i].login == loginText && listaKontow[i].password == pass) {
             listaKontow = listaKontow.filter(el => el.login !== loginText || el.password !== pass)
-            const sucsess = document.createElement("p")
-            sucsess.innerHTML = '<strong>Konto zostało usuniete</strong>'
-            sucsess.style.color = 'green'
-            LogRegForm.appendChild(sucsess)
-            setTimeout(()=>{sucsess.remove()},4000)
+            renderMessage("Konto zostalo usuniete", "green")
             foundAcc = true
             break
         }
     }
-    if (!foundAcc && !document.querySelector(".deleteAcc")){
-        deleteAccMessage()
+    if (!foundAcc && !document.querySelector(".deleteAcc")) {
+        renderMessage("Takiego konta nie istnieje", "red")
     }
     console.log(listaKontow)
 })
-function logining(){
+function logining() {
     const loginText = document.querySelector(".login").value
     const pass = document.querySelector(".password").value
-    for(let i in listaKontow){
-        if(listaKontow[i].login == loginText && listaKontow[i].password == pass){
+    let successLogin = false
+    if(loginAtt > 4){ //якщо 4 рази неправильно ввів дані
+        renderMessage("Przekroczono liczbę prób, sprobuj pozniej", "red")
+        logBtn.removeEventListener("click", logining) // видалення обробника кліку з кнопки
+        return
+    }
+    for (let i in listaKontow) {
+        if (listaKontow[i].login == loginText && listaKontow[i].password == pass) {
+            successLogin = true
             window.location.href = 'mainpage.html'
-        } else if(loginAtt > 4){ //якщо 4 рази неправильно ввів дані
-            alert("przekroczono liczbę prób, sprobuj pozniej")
-            logBtn.removeEventListener("click", logining) // видалення обробника кліку з кнопки
-        } else {
-            const error = document.createElement("p")
-            error.innerHTML = "Niepoprawny login lub haslo"
-            error.style = "color: red; font-weight: 900;"
-            LogRegForm.appendChild(error)
-            setTimeout(()=>{
-                error.remove()
-            },5000)
-            ++loginAtt
-        }
+        } 
+    }
+    if (!successLogin) { 
+        renderMessage("Niepoprawny login lub haslo", "red")
+        ++loginAtt
+        console.log(loginAtt)
     }
 }
 logBtn.addEventListener("click", logining) // після кліку на кнопку wejsc виконується функція вище
